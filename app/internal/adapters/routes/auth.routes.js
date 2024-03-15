3951637330c246c6c8f78d711f567edbf72fe8e9
Josema Pereira Cih <josema.pereira.cih@gmail.com>
2024-03-15 16:23:09 -0600

    feat: add auth documentation

--- app/internal/adapters/routes/auth.routes.js
+++ app/internal/adapters/routes/auth.routes.js
@@ -11,5 +11,23 @@ import { AuthController } from '../../core/controllers';
 export const AuthRoute = (app) => {
   const router = express.Router();
   app.use('/auth', router);
+
+  /**
+   * @swagger
+   * /auth/token:
+   *   get:
+   *     description: Returns token
+   *     tags:
+   *       - Auth
+   *     responses:
+   *       200:
+   *         description: A token
+   *         content:
+   *         application/json:
+   *          schema:
+   *            $ref: '#/components/schemas/AuthTokenResponse'
+   *       500:
+   *         $ref: '#/components/responses/InternalServerError'
+   */
   router.get('/token', AuthController.getToken);
 };
