d291326eb8c9c3beb55232a29951bbc0392cea9b
Josema Pereira Cih <josema.pereira.cih@gmail.com>
2024-03-22 15:46:50 -0600

    fix: users endpoint routes, services and controllers

--- app/internal/adapters/routes/users.routes.js
+++ app/internal/adapters/routes/users.routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { UsersController } from '../../core/controllers';
 
 /**
+ * {
  * Set up routes related to Users CRUD operations
  * @namespace Users
  * @function UsersRoute
@@ -12,12 +13,120 @@ export const UsersRoute = (app) => {
   const router = express.Router();
   app.use('/users', router);
 
-  // List by id
+  /**
+   * @swagger
+   * /users/{id}:
+   *   get:
+   *     description: Returns a user by id
+   *     parameters:
+   *       - in: path
+   *         name: id
+   *         schema:
+   *           type: string
+   *         required: true
+   *         description: ID of the user to get
+   *     security:
+   *       - bearerAuth: []
+   *     tags:
+   *       - Users
+   *     responses:
+   *       200:
+   *         description: User info
+   *         content:
+   *         application/json:
+   *          schema:
+   *            $ref: '#/components/schemas/DefaultResponse'
+   *       401:
+   *         $ref: '#/components/responses/UnauthorizedError'
+   *       500:
+   *         $ref: '#/components/responses/InternalServerError'
+   */
   router.get('/:id', UsersController.getById);
-  // Create a new user
+
+  /**
+   * @swagger
+   * /users:
+   *   post:
+   *     description: Create new user
+   *     requestBody:
+   *       description: User request body
+   *       required: true
+   *       content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/definitions/User'
+   *     tags:
+   *       - Users
+   *     responses:
+   *       200:
+   *         description: A User response
+   *         content:
+   *         application/json:
+   *          schema:
+   *            $ref: '#/components/schemas/UserResponse'
+   *       401:
+   *         $ref: '#/components/responses/UnauthorizedError'
+   *       500:
+   *         $ref: '#/components/responses/InternalServerError'
+   */
   router.post('/', UsersController.create);
-  // Edit User
+
+  /**
+   * @swagger
+   * /users/{id}:
+   *   put:
+   *     description: Update the user info by id
+   *     parameters:
+   *       - in: path
+   *         name: id
+   *         schema:
+   *           type: string
+   *         required: true
+   *         description: ID of the user to update
+   *     requestBody:
+   *       description: User request body
+   *       required: true
+   *       content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/definitions/User'
+   *     tags:
+   *       - Users
+   *     responses:
+   *       201:
+   *         description: User info
+   *         content:
+   *         application/json:
+   *          schema:
+   *            $ref: '#/components/schemas/DefaultResponse'
+   *       401:
+   *         $ref: '#/components/responses/UnauthorizedError'
+   *       500:
+   *         $ref: '#/components/responses/InternalServerError'
+   */
   router.put('/:id', UsersController.update);
-  // Delete user
+
+  /**
+   * @swagger
+   * /users/{id}:
+   *   delete:
+   *     description: Delete an user by id
+   *     parameters:
+   *       - in: path
+   *         name: id
+   *         schema:
+   *           type: string
+   *         required: true
+   *         description: ID of the user to delete
+   *     tags:
+   *       - Users
+   *     responses:
+   *       200:
+   *         $ref: '#/components/responses/Accepted'
+   *       401:
+   *         $ref: '#/components/responses/UnauthorizedError'
+   *       500:
+   *         $ref: '#/components/responses/InternalServerError'
+   */
   router.delete('/:id', UsersController.delete);
 };
